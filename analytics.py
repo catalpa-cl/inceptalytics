8dc4f0f7ba22097a1a6cb5b92dcc9cf60f6437f7
Marius Hamacher <marius.hamacher@uni-due.de>
2021-07-13 11:55:16 +0200

    annotation_analysis

---
+++ analytics.py
@@ -0,0 +1,69 @@
+import cassis
+from pathlib import Path
+from zipfile import ZipFile
+
+
+def load_typesystem(path):
+    with open(path, 'rb') as f:
+        return cassis.load_typesystem(f)
+
+
+def load_cas(path, typesystem):
+    with open(path, 'rb') as f:
+        return cassis.load_cas_from_xmi(f, typesystem)
+
+
+class Project:
+    def __init__(self, project_zip):
+        self.project_zip = project_zip
+        self._project_path = Path(project_zip)
+
+    def get_annotated_file_names(self) -> tuple:
+        complete_names = []
+        annotation_files = []
+        source_files = []
+
+        with ZipFile(self.project_zip) as zip_file:
+            for file in zip_file.namelist():
+                if file.startswith('annotation/'):
+                    complete_names.append(file)
+                    annotation_files.append(file.split('/')[-1])
+                    source_files.append(file.split('/')[1])
+        return complete_names, source_files, annotation_files
+
+    def extract_project_files(self, target_path: str, folder_name='annotation/'):
+        """Extracts zip files in the given project folder to the target path."""
+        with ZipFile(self.project_zip) as zip_file:
+            for file in zip_file.namelist():
+                if file.startswith(folder_name):
+                    zip_file.extract(file, target_path)
+
+    @property
+    def cas_objects(self):
+        annotation_dir = self._project_path/'annotation'
+
+        cases = []
+        for source_dir in annotation_dir.iterdir():
+            for annotator_dir in source_dir.iterdir():
+                ts_file = (annotator_dir/'TypeSystem.xml').resolve()
+                cas_file = next(annotator_dir.glob('*.xmi')).resolve()
+                cas = load_cas(cas_file, load_typesystem(ts_file))
+
+                cases.append(cas)
+
+        return cases
+
+
+    def annotations_of_layer(self, layer_name):
+        if len(layer_name.split('.')) == 1:
+            layer_name = f'webanno.custom.{layer_name}'
+
+        annotations = []
+        for cas in self.cas_objects:
+            try:
+                for annotation in cas.select(layer_name):
+                    annotations.append(annotation)
+            except cassis.typesystem.TypeNotFoundError:
+                continue
+
+        return annotations
