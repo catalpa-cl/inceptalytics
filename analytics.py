dbc488d8ff3b4ab53afa14394ed6eb84c7554b37
Marius Hamacher <marius.hamacher@uni-due.de>
2021-07-15 13:37:06 +0200

    split project into project and view.

--- analytics.py
+++ analytics.py
@@ -1,6 +1,7 @@
 import cassis
 import pandas as pd
 from itertools import combinations
+from functools import lru_cache
 from sklearn.metrics import cohen_kappa_score
 from krippendorff import alpha
 
@@ -11,10 +12,12 @@ class Project:
     @classmethod
     def from_zipped_xmi(cls, project_zip):
         annotations = annotation_info_from_xmi_zip(project_zip)
-        return cls(annotations)
+        return cls(annotations, project_zip, 'xmi')
 
-    def __init__(self, annotations):
+    def __init__(self, annotations, project_path, export_format):
         self._annotation_info = pd.DataFrame(annotations, columns=['cas', 'source_file', 'annotator'])
+        self.path = project_path
+        self.export_format = export_format
 
     @property
     def typesystem(self):
@@ -29,8 +32,8 @@ class Project:
         return self._unique_entries('annotator')
 
     @property
-    def cases(self):
-        return self._unique_entries('cas')
+    def cas_objects(self):
+        return self._annotation_info['cas'].tolist()
 
     def _unique_entries(self, info_type):
         return self._annotation_info[info_type].unique().tolist()
@@ -46,7 +49,12 @@ class Project:
 
         return df
 
-    def annotations(self, layer_name, annotators=None, source_files=None):
+    @lru_cache(maxsize=4)
+    def view(self, layer_name, feature_name=None, annotators=None, source_files=None):
+        level = 'layer' if feature_name is None else 'feature'
+        return View(self.annotations(layer_name, feature_name, annotators, source_files), self, level)
+
+    def annotations(self, layer_name, feature_name=None, annotators=None, source_files=None):
         layer_name = extend_layer_name(layer_name)
         relevant_annotations = self._filter_annotation_info(annotators, source_files).itertuples(index=False, name=None)
 
@@ -61,35 +69,44 @@ class Project:
 
         colnames = ['annotation', 'source_file', 'begin', 'end', 'annotator']
 
-        return pd.DataFrame(annotations, columns=colnames)
-
-    def annotation_matrix(self, layer_name, annotators=None, source_files=None):
         index = ['source_file', 'begin', 'end', 'annotator']
-        annotations = self.annotations(layer_name, annotators, source_files).set_index(index)
-        return annotations.loc[~annotations.index.duplicated(), 'annotation'].unstack()
+        annotations = pd.DataFrame(annotations, columns=colnames).set_index(index)
 
-    def feature_matrix(self, layer_name, feature_name, annotators=None, source_files=None):
-        annotations = self.annotation_matrix(layer_name, annotators, source_files)
-        return annotations.applymap(lambda x: x.get(feature_name), na_action='ignore')
+        if feature_name is not None:
+            annotations = annotations.applymap(lambda x: x.get(feature_name), na_action='ignore')
 
-    def feature_counts(self, layer_name, feature_name, grouped_by=None, annotators=None, source_files=None):
-        annotations = self.feature_matrix(layer_name, feature_name, annotators, source_files).stack()
+        return annotations
 
+
+class View:
+    def __init__(self, annotations, project, level='layer'):
+        self.annotations = annotations
+        self.level = level
+        self.project = project
+
+    @property
+    def document_annotator_matrix(self):
+        # TODO: handle more elegantly, annotations are lost by dropping duplicates
+        return self.annotations.loc[~self.annotations.index.duplicated(), 'annotation'].unstack()
+
+    def counts(self, grouped_by=None):
+        annotations = self.annotations
         if grouped_by is not None:
-            annotations = annotations.groupby(grouped_by)
-
+            annotations = self.annotations.groupby(grouped_by)
         return annotations.value_counts()
 
-    def iaa(self, layer_name, feature_name, measure='pairwise_kappa', level='nominal', annotators=None, source_files=None):
-        annotations = self.feature_matrix(layer_name, feature_name, annotators, source_files)
+    def iaa(self, measure='pairwise_kappa', level='nominal'):
+        if self.level == 'layer':
+            raise ValueError('Inter-Annotator Agreement is only implemented on "annotation" level.')
+
+        matrix = self.document_annotator_matrix
 
         if measure == 'pairwise_kappa':
-            annotators = annotations.columns
-
+            annotators = matrix.columns
             entries = []
             for pair in combinations(annotators, 2):
                 annotator_a, annotator_b = pair
-                data = annotations[list(pair)].dropna().values.T
+                data = matrix[list(pair)].dropna().values.T
                 n = data.shape[1]
 
                 score = cohen_kappa_score(data[0], data[1])
@@ -99,6 +116,6 @@ class Project:
             return pd.DataFrame(entries, columns=['a', 'b', 'n', 'kappa']).set_index(['a', 'b'])
 
         if measure == 'krippendorff':
-            categories = annotations.stack().unique()
+            categories = matrix.stack().unique()
             category_to_index = {category: i for i, category in enumerate(categories)}
-            return alpha(annotations.replace(category_to_index).values.T, level_of_measurement=level)
+            return alpha(matrix.replace(category_to_index).values.T, level_of_measurement=level)
