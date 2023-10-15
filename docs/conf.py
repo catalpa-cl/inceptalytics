72073d93fcb83862c05accf8350a088ae5388e30
Marius Hamacher <marius.hamacher@outlook.de>
2023-10-15 16:47:03 +0200

    change members in documentation to be grouped by type.

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-
 import sys
 from pathlib import Path
 sys.path.insert(0, str((Path(__file__).parent.parent.parent / 'inceptalytics').absolute()))
@@ -16,25 +7,17 @@ copyright = '2023, Marius Hamacher, Torsten Zesch'
 author = 'Marius Hamacher, Torsten Zesch'
 release = f'Version 0.1.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'm2r2',
 ]
 
+autodoc_member_order = 'groupwise'
 napoleon_google_docstring = True
-
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
 html_static_path = ['_static']
