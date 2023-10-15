791567b4c1afc47e471aff0277c9cac696142785
Marius Hamacher <marius.hamacher@outlook.de>
2023-10-15 14:22:43 +0200

    attempt to fix workflow.

--- docs/conf.py
+++ docs/conf.py
@@ -11,12 +11,10 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str((Path(__file__).parent.parent.parent / 'inceptalytics').absolute()))
 
-import inceptalytics
-
 project = 'INCEpTALYTICS'
 copyright = '2023, Marius Hamacher, Torsten Zesch'
 author = 'Marius Hamacher, Torsten Zesch'
-release = f'Version {inceptalytics.__version__}'
+release = f'Version 0.1.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
